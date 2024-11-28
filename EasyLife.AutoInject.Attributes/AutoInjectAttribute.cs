467a26eabe45cb2e31584ac9824f2470a922532a
Austin_Zhang <austin_zhang@resourcepro.com.cn>
2024-11-28 10:37:34 +0800

    Initial commit

---
+++ EasyLife.AutoInject.Attributes/AutoInjectAttribute.cs
@@ -0,0 +1,37 @@
+﻿namespace EasyLife.AutoInject.Attributes;
+
+public enum ServiceLifetime
+{
+    Singleton = 1,
+    Transient = 2,
+    Scoped = 4,
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+public class AutoInjectAttribute(Type baseType = null, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+    : System.Attribute
+{
+    public ServiceLifetime ServiceLifetime { get; set; } = serviceLifetime;
+
+    public Type BaseType { get; set; } = baseType;
+}
+
+
+#if NET7_0_OR_GREATER
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+public class AutoInjectAttribute<T>(ServiceLifetime serviceLifetime = ServiceLifetime.Scoped) : AutoInjectAttribute(typeof(T), serviceLifetime)
+{
+}
+
+#endif
+
+#if NET8_0_OR_GREATER
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+public class AutoInjectKeyedAttribute<T>(string key, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped) : AutoInjectAttribute(typeof(T), serviceLifetime)
+{
+    public string Key { get; set; } = key ?? throw new ArgumentNullException(nameof(key));
+}
+
+#endif
